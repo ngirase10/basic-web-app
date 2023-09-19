c47d7ac85f326fa92cc5370457ee12179cd354b5
ngirase10 <ngirase@andrew.cmu.edu>
2023-09-19 16:06:22 -0400

    add num largest

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -31,4 +31,9 @@ describe("QueryProcessor", () => {
         const response: string = QueryProcessor(query);
         expect(response).toBe(("jn"));
     });
+
+    test('should add numbers', () => {
+        expect(QueryProcessor("What is 5 plus 52?")).toEqual("57");
+        expect(QueryProcessor("What is 12 plus 1?")).toEqual("13");
+    });
 });
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -19,4 +19,18 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  const addMatch = query.match(/What is (\d+) plus (\d+)/);
+  if (addMatch) {
+    const x: number = parseInt(addMatch[1]);
+    const y: number = parseInt(addMatch[2]);
+    return (x+y).toString();
+  }
+
+  const largest = query.match(/Which of the following numbers is the largest: (\d), (\d), (\d)?/);
+  if (largest) {
+    const x: number = parseInt(largest[1]);
+    const y: number = parseInt(largest[2]);
+    const z: number = parseInt(largest[3]);
+    return ((Math.max(x,y,z)).toString());
+  }
   return "";}
